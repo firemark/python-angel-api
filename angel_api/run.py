8517452e16661499f1beb00d14582ad7bccd4e02
Firemark <marpiechula@gmail.com>
2014-03-27 00:46:31 +0100

    save exceptions to log file

--- angel_api/run.py
+++ angel_api/run.py
@@ -12,32 +12,32 @@ log = logging.getLogger("angelo-api")
 
 def run(start=1, continuous=False):
 
-    watchdog_counter = 0
-    requests_counter = 0
+    try:
+        log.info("Start.")
+        service = AngelService(start=start, continuous=continuous)
 
-    log.info("Start.")
-    service = AngelService(start=start, continuous=continuous)
+        for i in service.exiting_ids():
+            try:
+                resp = service.get(i)
+                service.add_to_db(i, resp)
 
-    for i in service.exiting_ids():
-        try:
-            resp = service.get(i)
-            service.add_to_db(i, resp)
+            except HTTPError as e:
+                http_resp = e.response
+                code = http_resp.status_code
+                log.error("HTTP Error (%i): %s", code, http_resp.json())
 
-        except HTTPError as e:
-            http_resp = e.response
-            code = http_resp.status_code
-            log.error("HTTP Error (%i): %s", code, http_resp.json())
+                if code in (408, 403):
+                    service.reset()
 
-            if code in (408, 403):
+            except TransportError as e:
+                log.error("ES Error (%i): %s", e.status_code, e.error)
+            except KeyboardInterrupt:
+                log.info("Keyboard interrupt :(")
                 service.reset()
-
-        except TransportError as e:
-            log.error("ES Error (%i): %s", e.status_code, e.error)
-
-        except KeyboardInterrupt:
-            log.info("Keyboard interrupt :(")
-            service.reset()
-            exit()
-        except Exception as e:
-            service.reset()
-            raise e
+                exit()
+            except:
+                service.reset()
+                raise
+    except:
+        log.exception('Unknow Error')
+        raise
